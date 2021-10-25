c820d4ca86f82e87912d90c5dd70901445817884
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-25 15:59:26 -0700

    started quest-data.js list

---
+++ data/quest-data.js
@@ -0,0 +1,80 @@
+const aasgard = {
+    id: 'aasgard', 
+    title: 'Trek Up Aasgard Pass', 
+    map: {}, 
+    image: , 
+    description: `You reach Aasgard Pass, and need to decide what to do as you approach. Remember, this pass has 2,230 ft elevation gain in under 1 mile... it will kick your BUTT. What do you do?!`, 
+    choices: [{
+        id: 'continue', 
+        description: 'Climb on!', 
+        result: `You are feeling fit, and have been training for this for a while! You decide to continue up the pass. You gain 20 instagram likes, but lose 10 clif bars for the effort`,
+        insta: 20, 
+        clif: -10, 
+    }, {
+        id: 'break', 
+        description: `Take a break`, 
+        result: `It is a hot day, and you are exhausted! You decide to take a break and swim at Colchuck Lake. You gain 10 instagram likes for the beautiful lake, and 5 clif bars for taking a break`, 
+        insta: 10, 
+        clif: 5,         
+    }, {
+        id: 'sos', 
+        description: `No way, call search & rescue`, 
+        result: `You are scared as hell. It is too far, and you forgot lunch. You call search and rescue, taking 10 instagram likes and 20 clif bars from your journey.`, 
+        insta: -10,
+        clif: -20, 
+    }]
+}; 
+
+const goats = {
+    id: 'goats', 
+    title: 'Encounter Mountain Goats!', 
+    map: {}, 
+    image: , 
+    description: , 
+    choices: [{
+        id: , 
+        description: , 
+        result: , 
+        insta: 
+        clif: 
+    }, {
+        id: , 
+        description: , 
+        result: , 
+        insta: 
+        clif:         
+    }, {
+        id: , 
+        description: , 
+        result: , 
+        insta: 
+        clif: 
+    }]
+}; 
+
+const wind = {
+    id: 'wind', 
+    title: 'Suffer A Wind Storm', 
+    map: {}, 
+    image: , 
+    description: , 
+    choices: [{
+        id: , 
+        description: , 
+        result: , 
+        insta: 
+        clif: 
+    }, {
+        id: , 
+        description: , 
+        result: , 
+        insta: 
+        clif:         
+    }, {
+        id: , 
+        description: , 
+        result: , 
+        insta: 
+        clif: 
+    }]
+}; 
