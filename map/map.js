f7fec39efceafa7ae3255c9126c046dcfc387ae9
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-25 17:33:22 -0700

    added map.js loop to update links on map html page

--- map/map.js
+++ map/map.js
@@ -4,6 +4,13 @@ import questArray from '../data/quest-data.js';
 // grab DOM elements 
 const mapLinks = document.getElementById('map-links'); 
 
+for (let quest of questArray){
+    const link = document.createElement('a'); 
+    link.href = `../quest/?id=${quest.id}`; 
+    link.textContent = quest.title; 
+    mapLinks.appendChild(link); 
+}
+
 // loop through each quest in quest-data array
 // in the loop, with each one, generate an <a> tag 
 // append the a tag with the quest ids as URL parameters
